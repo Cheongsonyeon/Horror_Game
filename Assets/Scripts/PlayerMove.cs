63fcdc2da3cab711ec0282d3d445ba161c4dff24
9Dollar <60121954+9Dollar@users.noreply.github.com>
2022-02-12 21:06:48 +0900

    완료

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 public class PlayerMove : MonoBehaviour
 {
-
-
     public CharacterController controller;
-
 
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
+    private bool isCrouching;
+    private bool isRunning;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -19,30 +18,38 @@ public class PlayerMove : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-    
+    [Header("HeadBob")]
+    [SerializeField] private float bobSpeed = 14f;
+    [SerializeField] private float bobAmount = .05f;
+    private float defaultYpos = 0;
+    private float Timer;
 
-  
-
-
+    private void Awake()
+    {
+        defaultYpos = Camera.main.transform.localPosition.y;
+    }
     private void Update()
     {
+        Movement();
 
+    }
+
+    void Movement()
+    {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-        float x = 0;
-        float z = 0;
 
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if((Input.GetButtonDown("Jump")) && isGrounded)
+        if ((Input.GetButtonDown("Jump")) && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
@@ -50,7 +57,18 @@ public class PlayerMove : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
-     
+        if(Mathf.Abs(move.x) >= .5f || Mathf.Abs(move.z) >= .5f)
+        {
+            HeadBob();
+        }
+    }
 
+    void HeadBob()
+    {
+        if (!isGrounded) return;
+
+
+        Timer += Time.deltaTime * bobSpeed;
+        Camera.main.transform.localPosition = new Vector3(Camera.main.transform.localPosition.x, defaultYpos + Mathf.Sin(Timer) * bobAmount, Camera.main.transform.localPosition.z);
     }
 }
